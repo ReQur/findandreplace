9a0e7fd3509bcc54f9d09d2d69682b0b5b46d3f9
Danil Khabibullin <dkhabibullin@cloudlinux.com>
2022-02-10 23:43:04 +0500

    INIT
    Add find function
    Add File Mask
    Add some interface(will be rework)

---
+++ Finder.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace findandreplace
+{
+    class Finder
+    {
+        public string Dir { get; set; }
+        public string FileMask { get; set; }
+        public string ExcludeFileMask { get; set; }
+        public string FindText { get; set; }
+        public bool IncludeFilesWithoutMatches { get; set; }
+        public bool IsSilent { get; set; }
+
+        public bool IsCancelRequested { get; set; }
+        public class FindResultItem : ResultItem
+        {
+        }
+        public class FindResult
+        {
+            public List<FindResultItem> Items { get; set; }
+            
+        }
+
+		public Finder()
+        {
+        }
+
+        public FindResult Find()
+        {
+
+            string[] filesInDirectory = Directory.GetFiles(Dir, FileMask, SearchOption.AllDirectories).ToArray();
+
+            var resultItems = new List<FindResultItem>();
+
+
+            foreach (string filePath in filesInDirectory)
+            {
+                
+                var resultItem = FindInFile(filePath);
+
+                //Skip files that don't have matches
+                if (resultItem.IncludeInResultsList)
+                    resultItems.Add(resultItem);
+
+            }
+
+
+            return new FindResult { Items = resultItems };
+        }
+
+		private FindResultItem FindInFile(string filePath)
+		{
+			var resultItem = new FindResultItem();
+			resultItem.IsSuccess = true;
+			//resultItem.IncludeFilesWithoutMatches = IncludeFilesWithoutMatches;
+
+			resultItem.FileName = Path.GetFileName(filePath);
+			resultItem.FilePath = filePath;
+			resultItem.FileRelativePath = "." + filePath.Substring(Dir.Length);
+
+            var fileText = File.ReadAllText(resultItem.FilePath);
+            if (fileText.Contains(FindText))
+            {
+                resultItem.NumMatches = 1;
+            }
+
+            return resultItem;
+		}
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace findandreplace
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private Finder _finder;
+        public MainViewModel()
+        {
+            Result = new ObservableCollection<ResultItem>();
+
+            FindCommand = new RelayCommand<string>(x =>
+            {
+                Result.Clear();
+                _finder = new Finder();
+                _finder.Dir = Dir;
+                _finder.FileMask = FileMask;
+                _finder.FindText = FindText;
+                
+                var res = _finder.Find();
+                foreach (var item in res.Items)
+                {
+                    Result.Add(item);
+                }
+            }, x => string.IsNullOrWhiteSpace(x) == false);
+        }
+        public ICommand FindCommand { get; }
+
+
+        private string _fileMask = "*.*";
+        public string FileMask
+        {
+            get => _fileMask;
+
+            set
+            {
+                if (_fileMask == value) return;
+
+                _fileMask = value;
+                OnPropertyChanged(nameof(_fileMask));
+            }
+        }
+        private string _dir = "";
+        public string Dir
+        {
+            get => _dir;
+
+            set
+            {
+                if (_dir == value) return;
+
+                _dir = value;
+                OnPropertyChanged(nameof(_dir));
+            }
+        }
+
+        private string _findText = "";
+
+        public string FindText
+        {
+            get => _findText;
+
+            set
+            {
+                if (_findText == value) return;
+
+                _findText = value;
+                OnPropertyChanged(nameof(_findText));
+            }
+        }
+
+        public ObservableCollection<ResultItem> Result { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+
+}
